8ad69e3789d767069687c5359deee9fdb0b4a87b
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-19 19:28:48 +0530

    Update store.js

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import loremReducer from "./reducer";
+import loremReducer from "../reducer";
 
 const store = createStore(loremReducer, applyMiddleware(thunk));
 
